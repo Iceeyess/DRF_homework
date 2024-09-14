388060a24fe58b9373237f3043de8e27c35cdabf
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-09-14 11:42:54 +0300

    fill_payments.py was modified.

--- users/management/commands/fill_payments.py
+++ users/management/commands/fill_payments.py
@@ -13,10 +13,11 @@ class Command(BaseCommand):
         # Create user
         params = dict(username='test', email='test@example.com', password='qwerty')
         user, user_status = User.objects.get_or_create(**params)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        print('User created successfully.')
+        if user_status:
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        print([f'User has already exists.', 'User was created successfully.'][user_status])
 
     # Create payments
         payment1 = {

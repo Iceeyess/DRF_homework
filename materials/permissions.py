21bd1a02294dd668e8fe8880336fd1e6b1d54102
Iceeyess <ice_eyes@mail.ru>
2024-09-24 09:04:40 +0300

    IsOwner has been changed logic for has_permission

--- materials/permissions.py
+++ materials/permissions.py
@@ -2,11 +2,11 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        if hasattr(view, 'get_object'):
-            obj = view.get_object()
-            print(1, request.user == obj.owner)
-            return request.user == obj.owner
+        # if hasattr(view, 'get_object'):
+        # obj = view.get_object()
+        print(1, request.user == view.queryset.model.objects.get(pk=view.kwargs.get('pk')).owner)
+        return request.user == view.queryset.model.objects.get(pk=view.kwargs.get('pk')).owner
 
     def has_object_permission(self, request, view, obj):
-        print(2, request.user == obj.owner)
-        return request.user == obj.owner
+        print(2, request.user == view.queryset.model.objects.get(pk=view.kwargs.get('pk')))
+        return request.user == view.queryset.model.objects.get(pk=view.kwargs.get('pk'))
